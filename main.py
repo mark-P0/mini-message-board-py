de5749fae5c0f8fb34c59f7a22efc9b84b1a7bf6
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2025-08-12 23:31:21 +0800

    build: add fastapi

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from mini-message-board-py!")
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return dict(hello="world")
 
 
 if __name__ == "__main__":
-    main()
+    ...
